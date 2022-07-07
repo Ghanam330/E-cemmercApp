6789dd0d5410f13a84c4b5a7414d0d5ab99e558c
yghna <yghnam334455@gmail.com>
2022-07-08 01:23:15 +0200

    homeScreen

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,8 +4,11 @@ import 'package:flutter/services.dart';
 
 class AppTheme {
   static Color lightBackgroundColor = const Color(0xfff2f2f2);
+
   static Color lightPrimaryColor = const Color(0xffff8900);
+
   static Color lightSecondaryColor = const Color(0xff040415);
+
   static Color lightAccentColor = Colors.blueGrey.shade200;
   static Color lightParticlesColor = const Color(0x44948282);
   static Color lightTextColor = Colors.black54;
