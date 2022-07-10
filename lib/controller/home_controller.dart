37c6ec00ee37a244754c8cde4a6ff2c669636ef9
yghna <yghnam334455@gmail.com>
2022-07-10 02:06:57 +0200

    homeScreen

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,22 +1,28 @@
 
+import 'package:ecemmercapp/model/product.dart';
 import 'package:ecemmercapp/service/remote_service/remote_popular_category_service.dart';
 import 'package:get/get.dart';
 import '../model/add_banner.dart';
 import '../model/category.dart';
 import '../service/remote_service/remote_banner_service.dart';
+import '../service/remote_service/remote_popular_product_service.dart';
 
 class HomeController extends GetxController {
   static HomeController instance = Get.find();
   RxList<AdBanner> bannerList = List<AdBanner>.empty(growable: true).obs;
   RxList<Category> popularCategoryList = List<Category>.empty(growable: true).obs;
+  RxList<Product> popularProductList = List<Product>.empty(growable: true).obs;
 
   RxBool isBannerLoading = false.obs;
   RxBool isPopularCategoryLoading = false.obs;
+
+  RxBool isPopularProductLoading = false.obs;
 
   @override
   void onInit() {
     getAdBanners();
     getPopularCategories();
+    getPopularProduct();
     super.onInit();
   }
 
@@ -45,6 +51,20 @@ class HomeController extends GetxController {
       print("Ahmed");
       print(popularCategoryList.length);
       isPopularCategoryLoading(false);
+    }
+  }
+
+  void getPopularProduct() async{
+    try{
+      isPopularProductLoading(true);
+      var result = await RemotePopularProductService().get();
+      if (result != null) {
+        popularProductList.assignAll(popularProductListFromJson(result.body));
+      }
+    } finally {
+      print("AhmedProduct");
+      print(popularProductList.length);
+      isPopularProductLoading(false);
     }
   }
 }
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,44 +1,60 @@
-import 'package:ecemmercapp/component/main_header.dart';
-import 'package:ecemmercapp/controller/controller.dart';
-import 'package:ecemmercapp/view/home/components/carousel_slider/carousel_loading.dart';
-import 'package:ecemmercapp/view/home/components/carousel_slider/carousel_slider_view.dart';
-import 'package:ecemmercapp/view/home/components/popular_category/section_title.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import '../../component/main_header.dart';
+import '../../controller/controller.dart';
+import 'components/carousel_slider/carousel_slider_view.dart';
 import 'components/popular_category/popular_category.dart';
 import 'components/popular_category/popular_category_loading.dart';
-import 'components/popular_category/popular_category_loading_card.dart';
+import 'components/carousel_slider/carousel_loading.dart';
+import 'components/popular_category/section_title.dart';
+import 'components/popular_product/popular_product.dart';
+import 'components/popular_product/popular_product_loading.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Column(
-            children:  [
-              const MainHeader(),
-              Obx(() {
-                if (homeController.bannerList.isNotEmpty) {
-                  return CarouselSliderView(bannerList:homeController.bannerList);
-                } else {
-                  return const CarouselLoading();
-                }
-              }
+          children: [
+            const MainHeader(),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    Obx(() {
+                      if (homeController.bannerList.isNotEmpty) {
+                        return CarouselSliderView(
+                            bannerList: homeController.bannerList);
+                      } else {
+                        return const CarouselLoading();
+                      }
+                    }),
+                    const SectionTitle(title: "Popular Category"),
+                    Obx(() {
+                      if (homeController.popularCategoryList.isNotEmpty) {
+                        return PopularCategory(
+                            categories: homeController.popularCategoryList);
+                      } else {
+                        return const PopularCategoryLoading();
+                      }
+                    }),
+                    const SectionTitle(title: "Popular Product"),
+                    Obx(() {
+                      if (homeController.popularProductList.isNotEmpty) {
+                        return PopularProduct(
+                            popularProducts: homeController.popularProductList);
+                      } else {
+                        return const PopularProductLoading();
+                      }
+                    }),
+                  ],
+                ),
               ),
-              const SectionTitle(title:"Popular Categories"),
-              Obx(() {
-                if (homeController.popularCategoryList.isNotEmpty) {
-                  return PopularCategory(
-                      categories: homeController.popularCategoryList
-                  );
-                } else {
-                  return const PopularCategoryLoading();
-                }
-              }
-              ),
-            ],
+            )
+          ],
         ));
   }
-
-
 }
